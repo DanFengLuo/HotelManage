2f24748b5c312e9660ccbe21422585bb4d12825e
zyp <759148450@qq.com>
2019-06-17 12:47:00 +0800

    客房信息管理优化，以及客房类型正则

--- src/components/rooms/edit.vue
+++ src/components/rooms/edit.vue
@@ -1,106 +1,143 @@
 <template>
   <div style="margin-top: 15px;">
-      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="客房类型" prop="roomTypeid">
-          <el-select v-model="ruleForm.roomTypeid" placeholder="客房类型">
-            <el-option v-for="entry in guestTypes" :label="entry.typeName" :value="entry.id" :key="entry.id"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="客房状态" prop="status">
-            <el-input   v-model="ruleForm.status"></el-input>
-        </el-form-item>
-        <el-form-item label="楼层" prop="floorId">
-        <el-select v-model="ruleForm.floorId" placeholder="请选择楼层信息">
-        <el-option v-for="entry in floors" :label="entry.floorName" :value="entry.id" :key="entry.id"></el-option>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="客房类型" prop="roomTypeid">
+        <el-select v-model="ruleForm.roomTypeid" placeholder="客房类型">
+          <el-option v-for="entry in guestTypes" :label="entry.typeName" :value="entry.id" :key="entry.id"></el-option>
         </el-select>
-        </el-form-item>
-        <el-form-item label="标准价格" prop="normalPrice">
-          <el-input   v-model="ruleForm.normalPrice"></el-input>
-        </el-form-item>
-        <el-form-item label="折后价" prop="discountPrice">
-          <el-input   v-model="ruleForm.discountPrice"></el-input>
-        </el-form-item>
-        <el-form-item label="普通会员价" prop="gvipPrice">
-          <el-input   v-model="ruleForm.gvipPrice"></el-input>
-        </el-form-item>
-        <el-form-item label="Vip会员价" prop="svipPrice">
-          <el-input   v-model="ruleForm.svipPrice"></el-input>
-        </el-form-item>
-        <el-form-item label="备注" prop="remarks">
-            <el-input type="textarea"  v-model="ruleForm.remarks"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm')">{{buttonText}}</el-button>
-            <el-button @click="resetForm('ruleForm')">重置</el-button>
-        </el-form-item>
-        </el-form>
+      </el-form-item>
+      <el-form-item label="客房状态" prop="status">
+        <el-select v-model="ruleForm.status" placeholder="请选择状态">
+          <el-option label="空房" :value="0"></el-option>
+          <el-option label="已预订" :value="1"></el-option>
+          <el-option label="入住" :value="2"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="楼层" prop="floorId">
+        <el-select v-model="ruleForm.floorId" placeholder="请选择楼层信息">
+          <el-option v-for="entry in floors" :label="entry.floorName" :value="entry.id" :key="entry.id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="标准价格" prop="normalPrice">
+        <el-input  v-model.number="ruleForm.normalPrice" v-model="normalPrice" size="medium" type="number"></el-input>
+      </el-form-item>
+      <el-form-item label="折后价" prop="discountPrice">
+        <el-input   v-model.number="ruleForm.discountPrice" v-model="discountPrice" size="medium" type="number"></el-input>
+      </el-form-item>
+      <el-form-item label="普通会员价" prop="gvipPrice">
+        <el-input   v-model.number="ruleForm.gvipPrice" size="medium" type="number"></el-input>
+      </el-form-item>
+      <el-form-item label="Vip会员价" prop="svipPrice">
+        <el-input   v-model.number="ruleForm.svipPrice" size="medium" type="number"></el-input>
+      </el-form-item>
+      <el-form-item label="备注" prop="remarks">
+        <el-input type="textarea"  v-model="ruleForm.remarks"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">{{buttonText}}</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
   export default {
-      inject:['reload'],
-      props:["id"],
+    inject:['reload'],
+    props:["id"],
     data () {
-        name:"roomsedit"
+      name:"roomsedit"
       return {
-          floors:[],
-          guestTypes:[],
-          ruleForm:{
-            id:"",
-            roomTypeid:"",
-            status:"",
-            floorId:"",
-            normalPrice:"",
-            discountPrice:"",
-            gvipPrice:"",
-            svipPrice:null,
-            remarks:""
-          },
-          rules: {
-//            roomType: [
-//                { required: true, message: '请输入商品名称', trigger: 'blur' },
-//                { min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: 'blur' }
-//            ]
+        floors:[],
+        guestTypes:[],
+        ruleForm:{
+          id:"",
+          roomTypeid:"",
+          status:"",
+          floorId:"",
+          normalPrice:"",
+          discountPrice:"",
+          gvipPrice:"",
+          svipPrice:"",
+          remarks:""
+        },
+        rules: {
+          roomTypeid: [
+            { required: true, message: '请选择客房类型', trigger: 'blur' },
+          ],
+          status: [
+            { required: true, message: '请选择客房状态', trigger: 'blur' },
+          ],
+          floorId: [
+            { required: true, message: '请选择客房楼层', trigger: 'blur' },
+          ],
+          normalPrice: [
+            { required: true,type: 'number',message: '请填写标准价格,只能输入数字', trigger: 'blur' },
+          ],
+//            discountPrice: [
+//              { required: true,type: 'number',message: '请填写折后价,只能输入数字', trigger: 'blur' },
+//            ],
+//            gvipPrice: [
+//              { required: true,type: 'number', message: '请填写普通会员价,只能输入数字', trigger: 'blur' },
+//            ],
+//            svipPrice: [
+//              { required: true,type: 'number', message: '请填写VIP会员价,只能输入数字', trigger: 'blur' },
+//            ],
         },
         buttonText:"创建"
 
       }
     },
     created(){
-        this.get("rooms/getAllFloor",(data)=>{
-          this.floors=data;
-        });
+      this.get("rooms/getAllFloor",(data)=>{
+        this.floors=data;
+      });
       this.get("rooms/getAllGuestType",(data)=>{
         this.guestTypes=data;
       });
-        if(this.id){
-             this.get("rooms/getOne",(data)=>{
-                this.ruleForm=data;
-                console.log(this.ruleForm);
-            },{id:this.id});
-            this.buttonText="修改"
-        }
+      if(this.id){
+        this.get("rooms/getOne",(data)=>{
+          this.ruleForm=data;
+          console.log(this.ruleForm);
+        },{id:this.id});
+        this.buttonText="修改"
+      }
     },
     components: {
 
     },
+//   双向绑定
+    watch : {
+      normalPrice:function(val) {
+        this.ruleForm.normalPrice = val;
+        this.ruleForm.discountPrice = this.ruleForm.normalPrice
+      },
+      discountPrice : function (val) {
+        this.ruleForm.discountPrice = val;
+      }
+    },
     methods:{
-        resetForm(formName){
-             this.$refs[formName].resetFields();
-        },
-        submitForm(formName){
-            let url="";
-            if(this.id){
-              url="rooms/update";
-              this.post(formName,url,this.ruleForm);
-            }
-            else{
-              url="rooms/add";
-              this.post(formName,url,this.ruleForm);
-            }
-
+      resetForm(formName){
+        this.$refs[formName].resetFields();
+      },
+      submitForm(formName){
+        let url="";
+        if(this.id){
+          url="rooms/update";
+          this.post(formName,url,this.ruleForm);
         }
+        else{
+          url="rooms/add";
+          this.post(formName,url,this.ruleForm);
+        }
+      },
+//        price(){
+//          let str=document.getElementById("normalPrice").value;
+//          document.getElementById("discountPrice").value = str;
+//          document.getElementById("gvipPrice").value = str;
+//          document.getElementById("svipPrice").value = str;
+//        }
+
     }
   }
 </script>
--- src/components/rooms/list.vue
+++ src/components/rooms/list.vue
@@ -4,17 +4,24 @@
       <el-row>
         <el-col :span="2"><el-button type="primary" @click="add">添加</el-button></el-col>
         <el-col :span="22">
-          <el-input placeholder="请输入客房编号" v-model="search.id" class="input-with-select" style="width: 200px">
+          <el-input placeholder="请输入客房编号" v-model="search.id" class="input-with-select" style="width: 340px">
+            <!--根据客房状态查询-->
+            <el-select v-model="search.status" style="width:120px" slot="prepend" placeholder="请选择状态">
+              <el-option label="全部" value=""></el-option>
+              <el-option label="空房" value="0"></el-option>
+              <el-option label="已预订" value="1"></el-option>
+              <el-option label="入住" value="2"></el-option>
+            </el-select>
             <el-button slot="append" icon="el-icon-search" @click="findData"></el-button>
           </el-input>
           <el-select v-model="search.floorId" filterable placeholder="请选择楼层信息" style="width: 200px"  @change="findData">
-          <el-option
-            v-for="item in floors"
-            :key="item.id"
-            :label="item.floorName"
-            :value="item.id">
-          </el-option>
-        </el-select>
+            <el-option
+              v-for="item in floors"
+              :key="item.id"
+              :label="item.floorName"
+              :value="item.id">
+            </el-option>
+          </el-select>
           <el-select v-model="search.roomTypeid" filterable placeholder="请选择类型信息" style="width: 200px"  @change="findData">
             <el-option
               v-for="item in guestTypes"
@@ -23,9 +30,10 @@
               :value="item.id">
             </el-option>
           </el-select>
+          <el-input placeholder="请输入价格" v-model="search.normalPrice" class="input-with-select" style="width: 200px">
+            <el-button slot="append" icon="el-icon-search" @click="findData"></el-button>
+          </el-input>
         </el-col>
-
-
 
       </el-row>
     </div>
@@ -44,7 +52,8 @@
       </el-table-column>
       <el-table-column
         prop="status"
-        label="客房状态">
+        label="客房状态"
+        :formatter="statusformat">
       </el-table-column>
       <el-table-column
         prop="floor.floorName"
@@ -66,15 +75,17 @@
         prop="svipPrice"
         label="Vip会员价">
       </el-table-column>
-      <el-table-column
-        prop="remarks"
-        label="备注">
-      </el-table-column>
+      <!--<el-table-column-->
+      <!--prop="remarks"-->
+      <!--label="备注">-->
+      <!--</el-table-column>-->
 
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button @click="edit(scope.row)" type="text" size="small">修改</el-button>
-          <el-button type="text" size="small" @click="del(scope.row)">{{deltext(scope.row.active)}}</el-button>
+          <!--添加设置，只有为空状态0 的时候才能修改和删除，否则隐藏按钮-->
+          <el-button @click="edit(scope.row)" type="text" size="small" v-if="scope.row.status==0">修改</el-button>
+          <el-button type="text" size="small" v-if="scope.row.status==0" @click="del(scope.row)">删除</el-button>
+          <el-button @click="detail(scope.row)" type="text" size="small">详情</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -91,6 +102,7 @@
 
 <script>
   import EditRooms from '@/components/rooms/edit'
+  import DetailRooms from '@/components/rooms/details'
   export default {
     inject:['reload'],
     name:"rooms",
@@ -99,14 +111,18 @@
         search:{
           id:"",
           floorId:"",
-          roomTypeid:""
+          roomTypeid:"",
+          normalPrice:"",
+          status:""
         },
         queryParams:{
           pageNo:1,
           pageSize:10,
           id:"",
           floorId:"",
-          roomTypeid:""
+          roomTypeid:"",
+          normalPrice:"",
+          status:""
         },
         tableData:{},
         floors:{},
@@ -137,6 +153,14 @@
           this.guestTypes=data;
         });
       },
+      statusformat(row, column, cellValue, index){
+        if(cellValue==0)
+          return "空房";
+        else if(cellValue==1)
+          return "已预订";
+        else if (cellValue==2)
+          return "入住"
+      },
       changePageNo(i){
         this.queryParams.pageNo=i;
       },
@@ -166,6 +190,19 @@
           },
           area:['800px','600px'],
           title: '修改客房信息',
+          shadeClose: false,
+          shade :true
+        });
+      },
+      detail(row){
+        this.$layer.iframe({
+          content: {
+            content: DetailRooms, //传递的组件对象
+            parent: this,//当前的vue对象
+            data:{id:row.id}//props
+          },
+          area:['800px','600px'],
+          title: '客房信息详情',
           shadeClose: false,
           shade :true
         });
