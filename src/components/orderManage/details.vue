2ca5e3d48c0e76e3e61032451e6a1a35616ca5e5
Vanilla <1135063038@qq.com>
2019-06-17 18:23:42 +0800

    预定信息详情页变为disabled

--- src/components/orderManage/details.vue
+++ src/components/orderManage/details.vue
@@ -2,43 +2,43 @@
   <div style="margin-top: 10px">
       <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="150px"  class="demo-ruleForm">
         <el-form-item label="预定编号" prop="id">
-           <el-input  v-model="ruleForm.id" readonly></el-input>
+           <el-input  v-model="ruleForm.id" disabled></el-input>
         </el-form-item>
         <el-form-item label="预定房间编号" prop="originalRoomId">
-           <el-input  v-model="ruleForm.originalRoomId" readonly></el-input>
+           <el-input  v-model="ruleForm.originalRoomId" disabled></el-input>
         </el-form-item>
         <el-form-item label="押金" prop="deposit">
-          <el-input  v-model="ruleForm.deposit" readonly></el-input>
+          <el-input  v-model="ruleForm.deposit" disabled></el-input>
         </el-form-item>
         <el-form-item label="预定人" prop="residents">
-          <el-input  v-model="ruleForm.residents" readonly></el-input>
+          <el-input  v-model="ruleForm.residents" disabled></el-input>
         </el-form-item>
         <el-form-item label="证件类型" prop="credentialsType">
-          <el-input  v-model="ruleForm.credentialsType" readonly></el-input>
+          <el-input  v-model="ruleForm.credentialsType" disabled></el-input>
         </el-form-item>
         <el-form-item label="证件号" prop="credentialsNum">
-          <el-input  v-model="ruleForm.credentialsNum" readonly></el-input>
+          <el-input  v-model="ruleForm.credentialsNum" disabled></el-input>
         </el-form-item>
         <el-form-item label="联系方式" prop="phone">
-          <el-input  v-model="ruleForm.phone" readonly></el-input>
+          <el-input  v-model="ruleForm.phone" disabled></el-input>
         </el-form-item>
         <el-form-item label="抵店时间" prop="arrivalTime">
-          <el-input  v-model="ruleForm.arrivalTime" readonly></el-input>
+          <el-input  v-model="ruleForm.arrivalTime" disabled></el-input>
         </el-form-item>
         <el-form-item label="离店时间" prop="leaveTime">
-          <el-input  v-model="ruleForm.leaveTime" readonly></el-input>
+          <el-input  v-model="ruleForm.leaveTime" disabled></el-input>
         </el-form-item>
         <el-form-item label="入住人数" prop="personNum">
-          <el-input  v-model="ruleForm.personNum" readonly></el-input>
+          <el-input  v-model="ruleForm.personNum" disabled></el-input>
         </el-form-item>
         <el-form-item label="会员编号" prop="memberId">
-          <el-input  v-model="ruleForm.memberId" readonly></el-input>
+          <el-input  v-model="ruleForm.memberId" disabled></el-input>
         </el-form-item>
         <!--        <el-form-item label="预定状态" prop="bookStatus">-->
         <!--            <el-input  v-model="ruleForm.bookStatus"></el-input>-->
         <!--        </el-form-item>-->
         <el-form-item label="备注" prop="remarks">
-          <el-input  v-model="ruleForm.remarks" readonly></el-input>
+          <el-input  v-model="ruleForm.remarks" disabled></el-input>
         </el-form-item>
         </el-form>
   </div>
@@ -51,7 +51,7 @@
     data () {
         name:"orderManagerdetails"
       return {
-          readonly: true,
+          disabled: true,
           ruleForm:{
             id:"",
             // orderManagename:"",
